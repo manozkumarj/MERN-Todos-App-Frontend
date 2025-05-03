a57a73fcce84179aca5515f48742ee7c363af812
manozkumarj <manozkumarj@users.noreply.github.com>
2025-05-03 04:23:55 +0000

    deploy: e27a6571ce18d513cb0d494230fe2cf41151edd2

--- assets/About-VQJ3nDIR.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CnXKeVAe.js";function o(){return t.jsx("div",{children:"This is About page"})}export{o as default};
--- assets/Settings-C-Ym01OH.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CnXKeVAe.js";function s(){return t.jsx("div",{children:"This is Settings page"})}export{s as default};
